a093baae419295f3b1f81d4a34e7a429386992c0
John O'Connor <oconnojb@gmail.com>
2018-06-05 16:15:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(x) {
+  if (x<=400) {
+    return "This one is on me!"
+  }
+
+  if (x > 2000 && x < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+
+  if (x >= 2500) {
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city) {
+  let answer = city === "NYC" ? "Ok, sounds good." : "No go."
+  return answer
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break
+  }
+}
