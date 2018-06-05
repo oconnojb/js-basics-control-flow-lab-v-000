cf65e8929b495d30c2a0545dce6660ef5b2752c6
John O'Connor <oconnojb@gmail.com>
2018-06-05 16:11:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ function scuberGreetingForFeet(x) {
   if (x<=400) {
     return "This one is on me!"
   }
-    
+
+  if (x > 2000 && x < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+
+  if (x > 2500) {
+    "No can do."
+  }
 }
